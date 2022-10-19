d0b25af04b8b3d6b60398063353e2118ed9d7c31
SpreedyGonzulus <92304002+SpreedyGonzulus@users.noreply.github.com>
2022-10-19 20:44:52 +0200

    Add files via upload

---
+++ starter/script.js
@@ -0,0 +1,21 @@
+const button1=document.getElementById("Burgir");
+const navigacja=document.getElementById("Navi");  
+
+const zegarek=document.getElementById("Clock");
+
+
+
+function wysun(){
+
+navigacja.classList.toggle("show");
+
+}
+
+
+
+
+
+
+button1.addEventListener("click",wysun)
+
+
