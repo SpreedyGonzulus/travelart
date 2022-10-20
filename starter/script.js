2257f6198add380de6b6d50a970835dd44401655
SpreedyGonzulus <92304002+SpreedyGonzulus@users.noreply.github.com>
2022-10-20 20:47:04 +0200

    Add files via upload

--- starter/script.js
+++ starter/script.js
@@ -3,6 +3,7 @@ const navigacja=document.getElementById("Navi");
 
 const zegarek=document.getElementById("Clock");
 
+const tlozeg=document.getElementById("tlozeg");
 
 
 function wysun(){
@@ -11,10 +12,32 @@ navigacja.classList.toggle("show");
 
 }
 
+function zegar(){
+    tlozeg.classList.toggle("show");
+}
+
+function interwal(){
+
+const currentDay=document.getElementById("Day");
+const day=new Date().toLocaleDateString('en-us',{weekday:'long'});
+
+currentDay.textContent=day;
+
+const currentHour=document.getElementById("Hour");
+const hour=new Date().getHours();
+const minutes=new Date().getMinutes();
+const seconds=new Date().getSeconds();
+
+currentHour.textContent=`${hour}:${minutes}:${seconds}`;
 
 
 
+}
 
+setInterval(interwal,1000);
+
+
+zegarek.addEventListener("click",zegar)
 
 button1.addEventListener("click",wysun)
 
